d72d00ac7263995dbbbc302255e4a79779989721
Fiera <46507472+Fiera42@users.noreply.github.com>
2024-11-26 14:31:26 +0100

    25% of collisions method

--- src/main/java/engine/agent/Action.java
+++ src/main/java/engine/agent/Action.java
@@ -2,4 +2,9 @@ package engine.agent;
 
 public record Action(double rotationRatio, double speedRatio) {
     // TODO: attention rotationRatio & speedRatio entre [-1, 1] uniquement
+
+    public Action {
+        rotationRatio = Math.clamp(rotationRatio, -1, 1);
+        speedRatio = Math.clamp(speedRatio, -1, 1);
+    }
 }
