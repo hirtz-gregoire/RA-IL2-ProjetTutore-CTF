3544078514422300e59d53185bcaa8207a825183
Fiera <46507472+Fiera42@users.noreply.github.com>
2024-11-26 13:52:10 +0100

    Run method

--- src/main/java/engine/Engine.java
+++ src/main/java/engine/Engine.java
@@ -6,6 +6,7 @@ import engine.agent.Agent;
 import engine.map.GameMap;
 import engine.object.GameObject;
 
+import java.time.Clock;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -15,18 +16,27 @@ public class Engine {
     private GameMap map;
     private List<GameObject> objects;
     private Display display;
+    private GameClock clock;
 
     private int tps = 20;
+    private int actualTps = 0;
 
     public Engine(List<Agent> agents, GameMap map, List<GameObject> objects, Display display) {}
     public Engine(List<Agent> agents, GameMap map, List<GameObject> objects) {}
 
     public void run(){
-        /*
-        loop tps
-            next()
-         */
+        clock = new GameClock();
+        long prevUpdate = -1;
+        int updateCount = 0;
 
+        while (true) {
+            if((Math.floor(clock.millis()) / 1000.0) % 1 == 1) actualTps = updateCount;
+            if(clock.millis() - prevUpdate < 1/tps) continue;
+
+            prevUpdate = clock.millis();
+            updateCount++;
+            next();
+        }
     }
 
     public void next(){
@@ -72,7 +82,7 @@ public class Engine {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    private void isGameFinished() {
+    private boolean isGameFinished() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
