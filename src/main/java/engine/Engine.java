e954280b0bff72f0a18f371cede7661148603e21
Fiera <46507472+Fiera42@users.noreply.github.com>
2024-11-26 12:49:05 +0100

    Next function

--- src/main/java/engine/Engine.java
+++ src/main/java/engine/Engine.java
@@ -4,23 +4,22 @@ import display.Display;
 import engine.agent.Action;
 import engine.agent.Agent;
 import engine.map.GameMap;
-import engine.object.Object;
+import engine.object.GameObject;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Engine {
 
     private List<Agent> agents;
     private GameMap map;
-    private List<Object> objects;
+    private List<GameObject> objects;
     private Display display;
 
     private int tps = 20;
 
-    public Engine(List<Agent> agents, GameMap map, List<Object> objects, Display display) {}
-    public Engine(List<Agent> agents, GameMap map, List<Object> objects) {}
+    public Engine(List<Agent> agents, GameMap map, List<GameObject> objects, Display display) {}
+    public Engine(List<Agent> agents, GameMap map, List<GameObject> objects) {}
 
     public void run(){
         /*
@@ -43,13 +42,33 @@ public class Engine {
         3. check fin simulation
         4. update affichage
         */
+
+        List<Agent> newAgents = new ArrayList<>();
+        List<GameObject> newObjects = new ArrayList<>(objects);
+
+        var actions = fetchActions();
+        var agentsCopy = new LinkedList<>(agents);
+        Collections.shuffle(agentsCopy);
+
+        while (!agentsCopy.isEmpty()) {
+            var agent = agentsCopy.removeFirst();
+            var action = actions.get(agent);
+            executeAction(agent, action, map, newAgents, newObjects);
+        }
+
+        var isGameFinished = isGameFinished();
+
+        agents = newAgents;
+        objects = newObjects;
+
+        display.update(map, agents, objects);
     }
 
     private Map<Agent, Action> fetchActions() {
         return this.agents.stream().parallel().collect(Collectors.toMap(agent -> agent,agent -> agent.getAction(this.map,this.agents,this.objects)));
     }
 
-    private void executeAction(Agent agent, Action action, GameMap map, List<Agent> agents, List<Object> objects) {
+    private void executeAction(Agent agent, Action action, GameMap map, List<Agent> agents, List<GameObject> objects) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
