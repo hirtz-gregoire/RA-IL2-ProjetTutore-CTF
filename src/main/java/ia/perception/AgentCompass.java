4f1fd0e2dc3f720d65cc7a5dd05a9753167b3628
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2024-12-17 10:37:36 +0100

    Modification of AgentCompass

--- src/main/java/ia/perception/AgentCompass.java
+++ src/main/java/ia/perception/AgentCompass.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AgentCompass extends Perception {
-    private Agent agent_suivi;
+    private final Agent agent_suivi;
     public AgentCompass(Agent a,Agent suivi) {
         super(a);
         this.agent_suivi = suivi;
@@ -19,17 +19,22 @@ public class AgentCompass extends Perception {
     @Override
     public PerceptionValue getValue(GameMap map, List<Agent> agents, List<GameObject> gameObjects) {
 
-        //calcul temps
         double x = getMy_agent().getCoordinate().x() - agent_suivi.getCoordinate().x();
         double y = getMy_agent().getCoordinate().y() - agent_suivi.getCoordinate().y();
         double distance = Math.sqrt((x * x) + (y * y));
-        double temps = distance / getMy_agent().getSpeed();
-        //calcul theta
+
+        double temps;
+        if(getMy_agent().getSpeed() == 0){
+            temps = Double.MIN_VALUE;
+        }
+        temps = distance / getMy_agent().getSpeed();
+
         double theta = Math.toDegrees(Math.atan(y / x));
-        //
+
         ArrayList<Double> vector = new ArrayList<Double>();
         vector.add(theta);
         vector.add(temps);
+
         if (agent_suivi.getTeam() != getMy_agent().getTeam()){
             return new PerceptionValue(PerceptionType.ENEMY, vector);
         }
