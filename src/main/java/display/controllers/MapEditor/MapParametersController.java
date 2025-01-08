ef4c796ccb2fb761b5fc7543d6e0951eca52a4db
TibereLN <tibere55190@gmail.com>
2025-01-08 15:56:16 +0100

    menu des paramètres de la carte

--- src/main/java/display/controllers/MapEditor/MapParametersController.java
+++ src/main/java/display/controllers/MapEditor/MapParametersController.java
@@ -1,6 +1,87 @@
 package display.controllers.MapEditor;
 
 import display.controllers.Controller;
+import display.model.MapEditorModel;
+import display.views.MapEditor.EnumMapEditor;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
 public class MapParametersController extends Controller {
+
+    @FXML
+    private Slider heightMapSlider;
+    @FXML
+    private Label heightMapLabel;
+    @FXML
+    private Slider widthMapSlider;
+    @FXML
+    private Label widthMapLabel;
+    @FXML
+    private Slider nbTeamSlider;
+    @FXML
+    private Label nbTeamLabel;
+
+    @FXML
+    public void initialize() {
+        heightMapSlider.setMin(1);
+        heightMapSlider.setMax(50);
+        heightMapSlider.setValue(10);
+        heightMapLabel.setText(String.valueOf(10));
+
+        widthMapSlider.setMin(1);
+        widthMapSlider.setMax(50);
+        widthMapSlider.setValue(10);
+        widthMapLabel.setText(String.valueOf(10));
+
+        nbTeamSlider.setMin(2);
+        nbTeamSlider.setMax(8);
+        nbTeamSlider.setValue(nbTeamSlider.getMin());
+        nbTeamLabel.setText(String.valueOf(nbTeamSlider.getMin()));
+    }
+
+    public void setHeightMap() {
+        MapEditorModel model = (MapEditorModel) this.model;
+        int height = (int) heightMapSlider.getValue();
+        model.setHeightMap(height);
+        heightMapLabel.setText(String.valueOf(height));
+        setNbTeamMax();
+    }
+
+    public void setWidthMap() {
+        MapEditorModel model = (MapEditorModel) this.model;
+        int width = (int) widthMapSlider.getValue();
+        model.setWidthMap(width);
+        widthMapLabel.setText(String.valueOf(width));
+        setNbTeamMax();
+    }
+
+    public void setNbTeam() {
+        MapEditorModel model = (MapEditorModel) this.model;
+        int nbTeam = (int) nbTeamSlider.getValue();
+        model.setNbTeam(nbTeam);
+        nbTeamLabel.setText(String.valueOf(nbTeam));
+    }
+
+    public void setNbTeamMax() {
+        int height = (int) heightMapSlider.getValue();
+        int width = (int) widthMapSlider.getValue();
+        int taille = height * width;
+        MapEditorModel model = (MapEditorModel) this.model;
+        int nbEquipesMax = Math.min(model.getGlobalModel().getNbTeamMax(), Math.round(taille/2));
+
+        nbTeamSlider.setMax(nbEquipesMax);
+        if (nbTeamSlider.getValue() > nbEquipesMax) {
+            nbTeamSlider.setValue(nbEquipesMax);
+            nbTeamLabel.setText(String.format("%.2f", nbEquipesMax));
+        }
+    }
+
+    public void mapCreator() {
+        MapEditorModel model = (MapEditorModel) this.model;
+        model.setActualMapEditorView(EnumMapEditor.MapCreator);
+
+        model.update();
+        model.getGlobalModel().updateRacine();
+    }
 }
--- src/main/java/display/model/MapEditorModel.java
+++ src/main/java/display/model/MapEditorModel.java
@@ -8,6 +8,10 @@ public class MapEditorModel extends ModelMVC {
 
     private EnumMapEditor actualMapEditorView = EnumMapEditor.Mode;
 
+    private int heightMap;
+    private int widthMap;
+    private int nbTeam;
+
     protected MapEditorModel(GlobalModel globalModel) {
         super(globalModel);
         update();
@@ -26,5 +30,28 @@ public class MapEditorModel extends ModelMVC {
     }
     public void setActualMapEditorView(EnumMapEditor actualMapEditorView) {
         this.actualMapEditorView = actualMapEditorView;
+    }
+    public int getWidthMap() {
+        return widthMap;
+    }
+
+    public void setWidthMap(int widthMap) {
+        this.widthMap = widthMap;
+    }
+
+    public int getNbTeam() {
+        return nbTeam;
+    }
+
+    public void setNbTeam(int nbTeam) {
+        this.nbTeam = nbTeam;
+    }
+
+    public int getHeightMap() {
+        return heightMap;
+    }
+
+    public void setHeightMap(int heightMap) {
+        this.heightMap = heightMap;
     }
 }
