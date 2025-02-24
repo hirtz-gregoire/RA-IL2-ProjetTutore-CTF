06f5dc5b66c21b4873f800eff68f155c13077f67
Fiera <46507472+Fiera42@users.noreply.github.com>
2025-02-24 20:07:18 +0100

    Stats

--- src/main/java/ia/ecj/statistics/CTF_CMAES_StatListener.java
+++ src/main/java/ia/ecj/statistics/CTF_CMAES_StatListener.java
@@ -2,5 +2,4 @@ package ia.ecj.statistics;
 
 public interface CTF_CMAES_StatListener {
     void postEvaluationStatistics(CTF_CMAES_Statistics.Stats[] stats);
-    void finalStatistics(CTF_CMAES_Statistics.Stats[] stats);
 }
